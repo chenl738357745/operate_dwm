253974a4ca4f0bf7306d9da9a3c7226f629d0524
DESKTOP-6VD3KBJ\73835 <738357745@qq.com>
2020-05-14 13:08:29 +0800

    no message

--- 华南供销存报表支持/衡泽-云徙联合项目20200513.sql
+++ 华南供销存报表支持/衡泽-云徙联合项目20200513.sql
@@ -42,10 +42,33 @@ BEGIN
     where w.APPROVAL_STATUS='已审核'
     order by objtype.OBJ_TYPE )
     
-   
+    ,可研 as(
+  --全盘:项目-业态顶级 
+    select ID,NAME,PARENT_ID
+    ,'' as 套,0 as 面积,0 as 金额,0 as 均价  from (
+    select id,project_name name,'可研' PARENT_ID,'' as 套,0 as 面积,0 as 金额,0 as 均价  from SYS_PROJECT
+    union all
+   --业态顶级id：项目id|业态顶级id
+    select proj.id||'|'||PRODUCT_TYPE.id as id
+    ,proj.PROJECT_NAME||'-'||PRODUCT_TYPE.PRODUCT_TYPE_NAME as name
+    ,proj.id as PARENT_ID
+    ,'' as 套,0 as 面积,0 as 金额,0 as 均价
+    from  sys_project proj cross join (select * from
+    MDM_BUILD_PRODUCT_TYPE where product_type_level=1) PRODUCT_TYPE
+    union all 
+    select 项目id||'|'||分期id||'|'||顶级业态id||'|'||末级业态id as id
+    ,末级业态全路径
+    ,项目id||'|'||分期id||'|'||顶级业态id as parent_id
+    ,'' as 套
+    ,总可售面积 as 面积
+    ,总货值 as 金额
+    ,0 as 均价 from basedata 
+    where 阶段类型=0))
+    
+    ,全盘 as(
     --全盘:项目-分期-业态顶级 
     select ID,NAME,PARENT_ID,'' as 套,0 as 面积,0 as 金额,0 as 均价  from (
-    select id,project_name name,null PARENT_ID  from SYS_PROJECT
+    select id,project_name name,'全盘' PARENT_ID  from SYS_PROJECT
     union all
     select proj_stage.PROJECT_ID||'|'||proj_stage.id  as Id
     ,proj_stage.STAGE_FULL_NAME as NAME
@@ -66,7 +89,22 @@ BEGIN
     ,总可售面积 as 面积
     ,总货值 as 金额
     ,0 as 均价 from basedata 
-    where 阶段类型=10;
+    where 阶段类型=10)
+    
+   select '可研' as id, '可研' as NAME,null PARENT_ID
+    ,'' as 套,0 as 面积,0 as 金额,0 as 均价 from dual
+   union all
+   select '全盘' as id,'全盘' as NAME,null PARENT_ID
+    ,'' as 套,0 as 面积,0 as 金额,0 as 均价 from dual
+   union all
+   select ID,NAME,PARENT_ID,套,面积,0金额,均价 from 可研
+   union all
+   select ID,NAME,PARENT_ID,套,面积,0金额,均价 from 全盘
+     ;
+
+    
+    
+    
     
 END P_DWM_云徙联合项目组;
 
