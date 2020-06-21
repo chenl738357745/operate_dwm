5b52762e22cf4d3593a691b029346f042e780680
DESKTOP-6VD3KBJ\73835 <738357745@qq.com>
2020-06-21 22:46:36 +0800

    no message

--- 目标货值/目标货值刷新20200621.sql
+++ 目标货值/目标货值刷新20200621.sql
@@ -1,25 +1,65 @@
 ----------------------------------------------chenl20200621开始------修改目标货值详情表主键长度
---alter table DWM_TARGET_WORTH_DTL add  WORTH_DTL_ID varchar2(80);
---alter table DWM_TARGET_WORTH_DTL add  WORTH_DTL_PARENT_ID varchar2(80);
---/
---COMMENT ON COLUMN "DWM_TARGET_WORTH_DTL"."WORTH_DTL_ID" IS '目标货值详情对象id';
---COMMENT ON COLUMN "DWM_TARGET_WORTH_DTL"."WORTH_DTL_PARENT_ID" IS '目标货值详情对象父级id';
-------------------------------------------------chenl20200621结束------修改目标货值详情表主键长度
+DECLARE COLUMNEXIST  NUMBER;
+BEGIN
+SELECT COUNT(*) INTO COLUMNEXIST  from user_tab_columns t where t.table_name='DWM_TARGET_WORTH_DTL' and t.column_name ='WORTH_DTL_ID';
+    IF COLUMNEXIST=0 THEN
+        EXECUTE IMMEDIATE 'alter table DWM_TARGET_WORTH_DTL add  WORTH_DTL_ID varchar2(80)';
+        EXECUTE IMMEDIATE 'COMMENT ON COLUMN "DWM_TARGET_WORTH_DTL"."WORTH_DTL_ID" IS ''目标货值详情对象id''';
+    END IF;
+end;
+/
+DECLARE COLUMNEXIST  NUMBER;
+BEGIN
+SELECT COUNT(*) INTO COLUMNEXIST  from user_tab_columns t where t.table_name='DWM_TARGET_WORTH_DTL' and t.column_name ='WORTH_DTL_PARENT_ID';
+    IF COLUMNEXIST=0 THEN
+        EXECUTE IMMEDIATE 'alter table DWM_TARGET_WORTH_DTL add  WORTH_DTL_PARENT_ID varchar2(80)';
+        EXECUTE IMMEDIATE 'COMMENT ON COLUMN "DWM_TARGET_WORTH_DTL"."WORTH_DTL_PARENT_ID" IS ''目标货值详情对象父级id''';
+    END IF;
+end;
+/
+COMMENT ON COLUMN "DWM_TARGET_WORTH_DTL"."OBJ_PARENT_ID" IS 'chenl20200621废弃字段,对象父级id';
+COMMENT ON COLUMN "DWM_TARGET_WORTH_DTL"."OLD_OBJ_ID" IS 'chenl20200621废弃字段,旧的对象id';
+
+----------------------------------------------chenl20200621结束------修改目标货值详情表主键长度
 ---------备份目标货值表
---create table DWM_TARGET_WORTH_DTL_cl  as select * from DWM_TARGET_WORTH_DTL;
+----drop table DWM_TARGET_WORTH_DTL_CL;
+----create table DWM_TARGET_WORTH_DTL_cl  as select * from DWM_TARGET_WORTH_DTL;
 ---------更新目标货值表历史数据
+--项目,分期,楼栋WORTH_DTL_ID=对象id+目标货值主表id
+--业态         WORTH_DTL_ID=id
+--项目,分期,楼栋,业态WORTH_DTL_PARENT_ID=OBJ_PARENT_ID+目标货值主表id
+update DWM_TARGET_WORTH_DTL set WORTH_DTL_ID=case when OBJ_TYPE=40 then id else obj_id||TARGET_WORTH_ID end where WORTH_DTL_ID is null;
+update DWM_TARGET_WORTH_DTL set WORTH_DTL_PARENT_ID=OBJ_PARENT_ID||TARGET_WORTH_ID where WORTH_DTL_PARENT_ID is null;
 --/
+--------目标货值详情，查询视图
+
 ----------------------------------------------更新重新计算存储过程
---create or replace PROCEDURE P_DWM_TARGET_WORTH_DTL_M (
---    targetWorthId         IN             VARCHAR2 --目标货值id
---) AS
-----修正目标货值错误数据存储修正。之前数据逻辑存储错误，保存时修改正。临时处理。
-----作者：陈丽
-----日期：2020/05/09
---BEGIN
---RETURN;
---END P_DWM_TARGET_WORTH_DTL_M;
---/
+create or replace PROCEDURE P_DWM_TARGET_WORTH_DTL_M (
+    targetWorthId         IN             VARCHAR2 --目标货值id
+) AS
+--修正目标货值错误数据存储修正。之前数据逻辑存储错误，保存时修改正。临时处理。
+--作者：陈丽
+--日期：2020/05/09
+BEGIN
+RETURN;
+
+with 计算 as(select (case when obj_type=40 then average_price else 0 end) as average_price_temp,
+    (case when obj_type=40 then nvl(average_price*(TOTAL_CARPORT+GROUND_CAN_SELL_AREA),0) else 0 end) as worth_temp,
+    id,target_worth_id,
+    worth_dtl_id,
+    worth_dtl_parent_id from DWM_TARGET_WORTH_DTL)
+
+ , 汇总 as( select s.*
+ ,(SELECT SUM(worth_temp) FROM 计算 START WITH ID=S.ID CONNECT BY PRIOR ID=obj_parent_id ) AS  worth_yuan
+ from 计算 s)
+
+SELECT 汇总.*
+,CASE WHEN (TOTAL_CARPORT+GROUND_CAN_SELL_AREA)=0 THEN 0 ELSE worth_yuan/(TOTAL_CARPORT+GROUND_CAN_SELL_AREA) END AS average_price
+,WORTH_yuan/10000 AS "worth"
+FROM 汇总;
+
+END P_DWM_TARGET_WORTH_DTL_M;
+/
 ----------------------------------------------chenl20200621开始------从主数据刷新目标货值存储过程（只是获取，未到数据库）
 create or replace PROCEDURE P_DWM_TARGET_WORTH_REF (
    USERID   IN   VARCHAR2,
@@ -291,7 +331,7 @@ left join MDM_OBJ_PHASE_INDEX pIndex on newph.instance_id =pIndex.OBJ_PHASE_ID)
         where (动态版本对象.OBJ_TYPE=30 )
 )
 
-, 计算 as(select pv.*,nvl(lw.AVERAGE_PRICE,0) as AVERAGE_PRICE_temp
+,计算 as(select pv.*,nvl(lw.AVERAGE_PRICE,0) as AVERAGE_PRICE_temp
 ,nvl(lw.AVERAGE_PRICE*(pv.TOTAL_CARPORT+pv.GROUND_CAN_SELL_AREA),0) as WORTH_temp
 from (
         -----可研
